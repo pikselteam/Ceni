921f1083aee930884ffa402f6b4851daebc54189
Filip Ristevski <fristevski@gmail.com>
2026-09-19 22:45:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import re  # За чистење на текстот од сметката
 # --- КОНФИГУРАЦИЈА НА GOOGLE GEMINI ---
 GEMINI_API_KEY = "AQ.Ab8RN6IED2bBzsP9jxgJJoc0VHNSfDt8gM7MBru5UxZIIm9Xnw"  # Замени го ова со твојот вистински клуч!
 
-if GEMINI_API_KEY != "AQ.Ab8RN6IED2bBzsP9jxgJJoc0VHNSfDt8gM7MBru5UxZIIm9Xnw">0:
+if GEMINI_API_KEY != "AQ.Ab8RN6IED2bBzsP9jxgJJoc0VHNSfDt8gM7MBru5UxZIIm9Xnw"
     genai.configure(api_key=GEMINI_API_KEY)
     model = genai.GenerativeModel('gemini-1.5-flash-latest')
 else:
